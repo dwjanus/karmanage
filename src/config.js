861cf95b6304488f2a9f09228612444927d0bebb
Devin Janus <devinjanus@DJanus-MBA-8GB.local>
2016-12-19 15:27:36 -0500

    fixed name of config vars

--- src/config.js
+++ src/config.js
@@ -10,8 +10,8 @@ const config = {
   MONGODB_URI: process.env.MONGODB_URI,
   PROXY_URI: process.env.PROXY_URI,
   ICON_EMOJI: ':robot:',
-  SLACK_CLIENT_ID: process.env.CLIENT_ID,
-  SLACK_CLIENT_SECRET: process.env.CLIENT_SECRET
+  SLACK_CLIENT_ID: process.env.SLACK_CLIENT_ID,
+  SLACK_CLIENT_SECRET: process.env.SLACK_CLIENT_SECRET
 }
 
 export default (key) => {
