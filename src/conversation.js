fb7d84b434080a7ecb90fa4f1ae23f096800e820
Devin Janus <devinjanus@DJanus-MBA-8GB.local>
2016-12-21 15:23:03 -0500

    Promise me all Ned

--- src/conversation.js
+++ src/conversation.js
@@ -30,7 +30,7 @@ export default (controller, bot) => {
   function mapUsers (userIds) {
     console.log(` --> mapUsers ---- userIds: ${userIds}`)
     return new Promise((resolve, reject) => {
-      let names = _.map(userIds, Promise.all(getUserName))
+      let names = _.map(userIds, Promise.resolve(getUserName))
       resolve(names)
     })
   }
