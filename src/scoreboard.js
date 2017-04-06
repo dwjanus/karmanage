e46328a5645d189ff95a6e83604fe875404df2da
Devin Janus <devinjanus@DJanus-MBA-8GB.local>
2017-04-06 15:26:27 -0400

    changed up our sort function, now using _.orderBy for team scoreboard

--- src/scoreboard.js
+++ src/scoreboard.js
@@ -19,7 +19,7 @@ async function scoreboard (leaderKarma, teamKarma) {
   }
 }
 
-function updateScoreboard (teamId) {
+function updateScoreboard (user) {
   storage.teams.get(user.team_id, (err, team) => {
     if (err) console.log(err)
     console.log(`Updating Scoreboard with user ${user.fullName} - ${user.karma}`)
