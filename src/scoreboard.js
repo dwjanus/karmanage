799b69830bed08056e440f11c17ab56da385a151
Devin Janus <devinjanus@DJanus-MBA-8GB.local>
2017-04-06 12:25:58 -0400

    changed up our sort function, now using _.orderBy for team scoreboard

--- src/scoreboard.js
+++ src/scoreboard.js
@@ -26,14 +26,14 @@ function updateScoreboard (user) {
     console.log('Updating Scoreboard')
     let checkScore = _.findIndex(teamKarma, (o) => { return o.name == user.name })
     console.log('checkScore: ' + checkScore)
-    if (checkScore == -1) teamKarma.push({name: user.name, score: user.karma})
+    if (checkScore === -1) teamKarma.push({name: user.name, score: user.karma})
     // if (teamKarma[checkScore]) {
     //   checkScore = _.findLastIndex(teamKarma, (o) => { o.name !== null || undefined })
     //   checkScore = checkScore + 1
     //   console.log('new checkScore: ' + checkScore)
     // }
     else teamKarma[checkScore].score = user.karma
-    team.scoreboard.karma = _.reverse(_.sortBy(teamKarma, [(o) => { return o.score }]))
+    team.scoreboard.karma = _.reverse(_.orderBy(teamKarma, ['score', 'name'], ['desc', 'asc']))
     console.log('Scoreboard Sorted by score:\n' + util.inspect(team.scoreboard.karma))
     storage.teams.save(team)
   })
