7313d50e8c17c6c4eb3d1bd722bb91ca7a73688d
Devin Janus <devinjanus@DJanus-MBA-8GB.local>
2017-04-19 15:39:19 -0400

    all works, polishing up and fixing slash commands

--- src/conversation.js
+++ src/conversation.js
@@ -233,7 +233,7 @@ export default (controller, bot) => {
                   text: `${team.name}: The Scorey So Far...`,
                   attachments: replyMessage.attachments
                 }
-                bot.replyInThread(message, replyMessage)
+                bot.replyPrivate(message, replyMessage)
               })
               .catch((err) => {
                 bot.replyPrivate(message, { text: err })
@@ -245,7 +245,7 @@ export default (controller, bot) => {
                   text: `${team.name}: The Scorey So Far...`,
                   attachments: replyMessage.attachments
                 }
-                bot.replyInThread(message, replyMessage)
+                bot.replyPrivate(message, replyMessage)
               })
               .catch((err) => {
                 bot.replyPrivate(message, { text: err })
