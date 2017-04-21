d2c9cc60653b4f9dc3e128a4b2154d3978b5f893
Devin Janus <devinjanus@DJanus-MBA-8GB.local>
2017-04-21 15:40:00 -0400

    first push to new staging branch

--- src/config.js
+++ src/config.js
@@ -7,10 +7,10 @@ if (ENV === 'development') dotenv.load()
 const config = {
   ENV: process.env.NODE_ENV,
   PORT: process.env.PORT,
-  MONGODB_URI: process.env.MONGODB_URI,
+  MONGODB_URI: process.env.MONGODB_DEV_URI,
   PROXY_URI: process.env.PROXY_URI,
-  SLACK_CLIENT_ID: process.env.SLACK_CLIENT_ID,
-  SLACK_CLIENT_SECRET: process.env.SLACK_CLIENT_SECRET
+  SLACK_CLIENT_ID: process.env.SLACK_CLIENT_ID_DEV,
+  SLACK_CLIENT_SECRET: process.env.SLACK_CLIENT_SECRET_DEV
 }
 
 export default (key) => {
--- src/index.js
+++ src/index.js
@@ -35,12 +35,12 @@ controller.setupWebserver(port, (err, webserver) => {
   controller.createWebhookEndpoints(controller.webserver)
   controller.createOauthEndpoints(controller.webserver, (err, req, res) => {
     if (err) res.status(500).send(`ERROR: ${err}`)
-    else res.redirect('https://karmanage.herokuapp.com/success')
+    else res.redirect('https://karmanage-stage.herokuapp.com/success')
   })
 
   webserver.get('/', (req, res) => {
-    res.send('<a href="https://slack.com/oauth/authorize?scope=incoming-webhook,' +
-      'commands,bot&client_id=64177576980.117306046992"><img alt="Add to Slack" ' +
+    res.send('<a href="https://slack.com/oauth/authorize?scope=' +
+      'commands,bot&client_id=64177576980.171646816545"><img alt="Add to Slack" ' +
       'height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" ' +
       'srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x,' +
       'https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>')
@@ -172,5 +172,5 @@ const buildscores = (teamId) => {
 
 // Simple hack to ping server every 5min and keep app running
 setInterval(() => {
-  http.get('http://karmanage.herokuapp.com')
+  http.get('http://karmanage-stage.herokuapp.com')
 }, 300000)
