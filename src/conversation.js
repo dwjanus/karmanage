5ffb77aba232c43f7e1b8c3a16d402c1ffb6cabe
Devin Janus <devinjanus@DJanus-MBA-8GB.local>
2017-05-04 13:54:10 -0400

    added static folder and updated help a little bit

--- src/conversation.js
+++ src/conversation.js
@@ -85,10 +85,10 @@ export default (controller, bot) => {
         color: '#009999',
         text: '_/mykarma_ - for your individual score\n' +
               '_/scoreboard_ - to see where you stack up\n',
-        footer: 'Devin Janus | Karmabot - v. 1.0',
+        footer: 'Devin Janus  |  Karmabot - v. 1.0 |',
         footer_icon: 'https://karmanage.herokuapp.com/public/images/smashing-emoji.png'
         ts: 123456789,
-        mrkdwn_in: ['text', 'pretext', ]
+        mrkdwn_in: ['text', 'pretext']
       }
     ]
 
