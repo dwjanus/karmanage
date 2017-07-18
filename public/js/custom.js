4b8cedb2edb35ffb8df63776ca92c1fcaf08629d
Devin Janus <devinjanus@D.Janus-MBP-8GB>
2017-07-18 16:55:12 -0400

    landing page works, making it look good now

--- public/js/custom.js
+++ public/js/custom.js
@@ -84,19 +84,6 @@ $(document).ready(function () {
             $(this).unbind('inview');
         }
     });
-    /***************** Google Map ******************/
-
-    function initialize() {
-        var mapCanvas = document.getElementById('map');
-        var mapOptions = {
-            center: new google.maps.LatLng(39.92757, -83.160207),
-            zoom: 8,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-        var map = new google.maps.Map(mapCanvas, mapOptions);
-    }
-
-    google.maps.event.addDomListener(window, 'load', initialize);
 
     /***************** Wow.js ******************/
 
@@ -110,51 +97,47 @@ $(document).ready(function () {
     });
 
     /***************** Parallax *****************/
-    // makes the parallax elements
-    function parallaxIt() {
 
-      // create variables
-      var $fwindow = $(window);
-      var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    // create variables
+    var $fwindow = $(window);
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-      // on window scroll event
+    // on window scroll event
+    $fwindow.on('scroll resize', function() {
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    });
+
+    // for each of content parallax element
+    $('[data-type="content"]').each(function (index, e) {
+      var $contentObj = $(this);
+      var fgOffset = parseInt($contentObj.offset().top);
+      var yPos;
+      var speed = ($contentObj.data('speed') || 1 );
+
+      $fwindow.on('scroll resize', function (){
+        yPos = fgOffset - scrollTop / speed;
+
+        $contentObj.css('top', yPos);
+      });
+    });
+
+    // for each of background parallax element
+    $('[data-type="background"]').each(function(){
+      var $backgroundObj = $(this);
+      var bgOffset = parseInt($backgroundObj.offset().top);
+      var yPos;
+      var coords;
+      var speed = ($backgroundObj.data('speed') || 0 );
+
       $fwindow.on('scroll resize', function() {
-        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        yPos = - ((scrollTop - bgOffset) / speed);
+        coords = '40% '+ yPos + 'px';
+
+        $backgroundObj.css({ backgroundPosition: coords });
       });
+    });
 
-      // for each of content parallax element
-      $('[data-type="content"]').each(function (index, e) {
-        var $contentObj = $(this);
-        var fgOffset = parseInt($contentObj.offset().top);
-        var yPos;
-        var speed = ($contentObj.data('speed') || 1 );
+    // triggers winodw scroll for refresh
+    $fwindow.trigger('scroll');
 
-        $fwindow.on('scroll resize', function (){
-          yPos = fgOffset - scrollTop / speed;
-
-          $contentObj.css('top', yPos);
-        });
-      });
-
-      // for each of background parallax element
-      $('[data-type="background"]').each(function(){
-        var $backgroundObj = $(this);
-        var bgOffset = parseInt($backgroundObj.offset().top);
-        var yPos;
-        var coords;
-        var speed = ($backgroundObj.data('speed') || 0 );
-
-        $fwindow.on('scroll resize', function() {
-          yPos = - ((scrollTop - bgOffset) / speed);
-          coords = '40% '+ yPos + 'px';
-
-          $backgroundObj.css({ backgroundPosition: coords });
-        });
-      });
-
-      // triggers winodw scroll for refresh
-      $fwindow.trigger('scroll');
-    };
-
-    parallaxIt();
 })
