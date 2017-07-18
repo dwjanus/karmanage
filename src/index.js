f3087cdf0b136fd8da9a4a8048abd5f9a6824289
Devin Janus <devinjanus@D.Janus-MBP-8GB>
2017-07-18 14:08:08 -0400

    trying to make the landing page okay

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ controller.setupWebserver(port, (err, webserver) => {
 
   webserver.get('/', (req, res) => {
     // res.send('<a href="https://slack.com/oauth/authorize?&client_id=64177576980.171646816545&scope=bot,commands"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>')
-    res.sendFile('/static/index.html')
+    res.sendFile(path.join(__dirname + '/static/index.html'))
   })
 
   webserver.get('/success', (req, res) => {
